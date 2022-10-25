624cb431b33d923961874330f1ff4d031635be0d
TreleaniFederico <treleani.federico@gmail.com>
2022-10-25 14:52:56 +0200

    add bootstrap cdn add html sections

--- php_mysql.php
+++ php_mysql.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.2/css/bootstrap.min.css" integrity="sha512-CpIKUSyh9QX2+zSdfGP+eWLx23C8Dj9/XmHjZY2uDtfkdLGo0uY12jgcnkX9vXOgYajEKb/jiw67EYm+kBf+6g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>PHP MYSQL</title>
 </head>
 <body>
     
@@ -23,13 +24,39 @@
 
         echo('Connection OK!');
 
-        $sql = 'SELECT `name`, `surname`, `email` FROM `students`';
+        $sql = 'SELECT `students`.`surname`, `students`.`name`, `degrees`.`name` AS `degree_name`, `departments`.`name` AS `department_name` FROM `students` JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id` JOIN `departments` ON `degrees`.`department_id` = `departments`.`id` GROUP BY `students`.`surname`, `students`.`name`, `degrees`.`name`, `departments`.`name` LIMIT 100';
 
         $result = $conn -> query($sql);
     
         if($result && $result -> num_rows > 0){
+
+            ?>
+                    <div class="row">
+                        <div class="col-2 text-center"><h4>SURNAME</h4></div>
+                        <div class="col-2 text-center"><h4>NAME</h4></div>
+                        <div class="col text-center"><h4>CORSO DI LAUREA</h4></div>
+                        <div class="col text-center"><h4>DIPARTIMENTO</h4></div>
+                    </div>
+            <?php
+
             while($row = $result -> fetch_assoc()){
-                
+
+                ?>
+                    <div class="row">
+                        <div class="col-2 text-center">
+                            <p> <?= $row['surname']  ?> </p>
+                        </div>
+                        <div class="col-2 text-center">
+                            <p> <?= $row['name']  ?> </p>
+                        </div>
+                        <div class="col text-center">
+                            <p> <?= $row['degree_name']  ?> </p>
+                        </div>
+                        <div class="col text-center">
+                            <p> <?= $row['department_name']  ?> </p>
+                        </div>
+                    </div>
+                <?php
             }
         }
 
