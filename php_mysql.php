b5e6bbfd5415cb227728ae9229fa89814b0094b8
TreleaniFederico <treleani.federico@gmail.com>
2022-10-25 09:40:38 +0200

    add define parameters

--- php_mysql.php
+++ php_mysql.php
@@ -1,6 +1,23 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        define("DB_SERVERNAME", "localhost:8889");
+        define("DB_USERNAME","root");
+        define("DB_PASSWORD", "root");
+        define("DB_NAME", "db_university");
 
 
 
+    ?>
 
-?>
+</body>
+</html>
+
